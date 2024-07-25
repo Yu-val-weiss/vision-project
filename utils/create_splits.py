79ed2bcddf14372eeee01ea9cf8817155fee3db3
yu-val-weiss <56967322+Yu-val-weiss@users.noreply.github.com>
2024-07-25 17:03:16 +0100

    remove dev split

--- utils/create_splits.py
+++ utils/create_splits.py
@@ -6,10 +6,10 @@ from utils.config import COLLECTED_DATA_CSV, TRAIN_CSV, DEV_CSV, TEST_CSV
 df = pd.read_csv(COLLECTED_DATA_CSV)
 
 # Shuffle and split the data
-train, temp = train_test_split(df, test_size=0.2, random_state=42)
-dev, test = train_test_split(temp, test_size=0.5, random_state=42)  
+train, test = train_test_split(df, test_size=0.2, random_state=42)
+# dev, test = train_test_split(temp, test_size=0.5, random_state=42)  
 
 # Save the splits
 train.to_csv(TRAIN_CSV, index=False)
-dev.to_csv(DEV_CSV, index=False)
+# dev.to_csv(DEV_CSV, index=False)
 test.to_csv(TEST_CSV, index=False)
