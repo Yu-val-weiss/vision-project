e17d3b084b8078bca1e3491a96f5704176e92ef5
yu-val-weiss <56967322+Yu-val-weiss@users.noreply.github.com>
2024-07-25 14:57:05 +0100

    add dataset creation capabilities

--- main.py
+++ main.py
@@ -3,10 +3,26 @@ import time
 import cv2 as cv
 import numpy as np
 from utils.annotator import Annotator
-from utils.config import MODEL_ASSET_PATH
+from utils.config import MODEL_ASSET_PATH, COLLECTED_DATA_CSV
+from utils.data import check_class_proportions
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
+import csv
+
+
+class_dict = {
+    ')': 10,
+    '!': 11,
+    '@': 12,
+    '£': 13,
+    '$': 14,
+    '%': 15,
+    '^': 16,
+    '&': 17,
+    '*': 18,
+    '(': 19
+}
 
 
 annotator = Annotator()
@@ -23,6 +39,10 @@ def process_res(result: vision.HandLandmarkerResult, output_image: mp.Image, tim
     except Exception as e:
         print(f"Error processing results: {e}")
     
+def record_datapoint(class_index, embedding: np.ndarray):
+    with open(COLLECTED_DATA_CSV, 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow([class_index] + embedding.reshape(-1).tolist())
 
 if __name__ == '__main__':
     cap = cv.VideoCapture(0) # 0 sets to default camera (index)
@@ -65,16 +85,44 @@ if __name__ == '__main__':
             
             
             # print(embed_list)
+            if record_mode:
+                res_img = cv.putText(res_img,"Record mode", (30,30), cv.FONT_HERSHEY_DUPLEX,1,(255,0,0))
             
             cv.imshow('frame', res_img)
             
             # wait for...
             key = cv.waitKey(1)
             if key == ord('q'):
+                if record_mode:
+                    print('Quit in record mode, printing class proportions...')
+                    check_class_proportions()
                 break
+            
             elif key == ord('r'):
                 record_mode = not record_mode
-                print(f"{'Entered' if record_mode else 'Exited'} record mode")
+                print(f"\n\n{'!!  Entered' if record_mode else 'Exited'} record mode  !!\n")
+                print('Class proportions...')
+                check_class_proportions()
+                    
+            elif key != -1:
+                if record_mode:
+                    if len(embed_list) == 0:
+                        print('No hands found to record')
+                        continue
+                    if len(embed_list) > 1:
+                        print('More than one hand found, skipped recording')
+                        continue
+                    c = chr(key)
+                    if c.isdigit():
+                        for emb in embed_list:
+                            record_datapoint(int(c), emb)
+                        print(f'Recorded {int(c)}')
+                    elif c in class_dict:
+                        for emb in embed_list:
+                            record_datapoint(class_dict[c], emb)
+                        print(f'Recorded {class_dict[c]}')
+                    else:
+                        print(f"Invalid key {c}")
             
             
             
